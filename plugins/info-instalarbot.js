9346e3c97d73b529a03336f2eb34dd0ce7b6c826
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-18 23:51:14 -0400

    Actualizar info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*Hola si deseas comprar el bot habla a los siguientes numeros*
-https://walink.co/62efd1
-https://walink.co/6ecb77`.trim();
+  const text = `*Hola si deseas instalar el bot*
+wa.me/59897246324
+wa.me/59897246324`.trim();
   const buttonMessage= {
-    'document': {url: `https://www.instagram.com/ceogeripium.dzn`},
+    'document': {url: `https://www.instagram.com/usxr_angelito`},
     'mimetype': `application/${document}`,
     'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
     'fileLength': 99999999999999,
@@ -17,10 +17,10 @@ https://walink.co/6ecb77`.trim();
         'mediaUrl': 'https://www.instagram.com/ceogeripium.dzn',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+        'title': 'ꜱɪɢᴜᴇᴍᴇ ᴇɴ ɪɴꜱᴛᴀɢʀᴀᴍ',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.instagram.com/ceogeripium.dzn'}},
+        'sourceUrl': 'https://www.instagram.com/usxr_angelito'}},
     'caption': text,
     'footer': wm,
     // 'buttons':[
@@ -29,5 +29,5 @@ https://walink.co/6ecb77`.trim();
     'headerType': 6};
   conn.sendMessage(m.chat, buttonMessage, {quoted: m});
 };
-handler.command = ['comprarbot', 'adquirirbot'];
+handler.command = ['instalarbot', 'botinstalar'];
 export default handler;
