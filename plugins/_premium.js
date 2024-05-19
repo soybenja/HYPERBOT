35d10c9f4915dfc1cfb0833b2489452824f1d042
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-19 00:32:58 -0400

    Actualizar _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -8,7 +8,7 @@ export async function all(m) {
         user.premium = false;
         const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user);
         const usuarioJid = JID.split`@`[0];
-        const textoo = `*[❗] @${usuarioJid} 𝚃𝚄 𝚃𝙸𝙴𝙼𝙿𝙾 𝙲𝙾𝙼𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝙷𝙰 𝙴𝚇𝙿𝙸𝚁𝙰𝙳𝙾, 𝚈𝙰 𝙽𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`;
+        const textoo = `🔮 @${usuarioJid} 𝙏𝙪 𝙩𝙞𝙚𝙢𝙥𝙤 𝙘𝙤𝙢𝙤 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙥𝙧𝙚𝙢𝙞𝙪𝙢 𝙖 𝙩𝙚𝙧𝙢𝙞𝙣𝙖𝙙𝙤. 𝙔𝙖 𝙣𝙤 𝙚𝙧𝙚𝙨 𝙥𝙧𝙚𝙢𝙞𝙪𝙢`;
         await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: ''});
       }
     }
@@ -25,6 +25,6 @@ export async function all(m) {
     user.premiumTime = 0
     user.premium = false
 
-    await m.reply(`*[❗] @${m.sender.split`@`[0]} 𝚃𝚄 𝚃𝙸𝙴𝙼𝙿𝙾 𝙲𝙾𝙼𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝙷𝙰 𝙴𝚇𝙿𝙸𝚁𝙰𝙳𝙾, 𝚈𝙰 𝙽𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`, m.sender, { mentions: [m.sender] })
+    await m.reply(`🔮 @${m.sender.split`@`[0]} 𝙏𝙪 𝙩𝙞𝙚𝙢𝙥𝙤 𝙘𝙤𝙢𝙤 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙥𝙧𝙚𝙢𝙞𝙪𝙢 𝙖 𝙩𝙚𝙧𝙢𝙞𝙣𝙖𝙙𝙤. 𝙔𝙖 𝙣𝙤 𝙚𝙧𝙚𝙨 𝙥𝙧𝙚𝙢𝙞𝙪𝙢`, m.sender, { mentions: [m.sender] })
   }
 }*/
