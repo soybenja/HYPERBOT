fbb9fdbecc54f38e480805a7b5e75af9d1d9eadc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-18 22:58:37 -0400

    Actualizar menuff.js

--- plugins/menuff.js
+++ plugins/menuff.js
@@ -18,7 +18,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `┏━━━•💌 𝙍𝙪𝙗𝙮𝘽𝙤𝙩-𝙈𝘿 💌•━━━━┓
+    const str = `┏━━━━•💌 𝙓𝙞𝙖𝘽𝙤𝙩-𝙈𝘿 💌•━━━━┓
 │╭─────────♡─────────╮
 ││•💜¡𝙃𝙊𝙇𝘼! @${m.sender.split`@`[0]}
 │╰─────────♡─────────╯
