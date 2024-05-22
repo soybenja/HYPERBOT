a3aa460cf31954f3fc762ef25226f4b7038730c4
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-23 02:54:44 +1300

    Update gc-setdesc.js

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*✅ La descripción del grupo se modifico correctamente*');
+  m.reply('❗ *𝙃𝙔𝙋𝙀𝙍´𝙑 𝘽𝙊𝙏* 𝙈𝙊𝘿𝙄𝙁𝙄𝘾𝙊 𝙇𝘼 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝘾𝙊𝙉 𝙀𝙓𝙄𝙏𝙊');
 };
 handler.help = ['Setdesc <text>'];
 handler.tags = ['group'];
