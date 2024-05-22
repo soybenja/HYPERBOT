96d286f2c65c979510f1b9599b076082eb50c154
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-23 04:13:16 +1300

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -7,12 +7,12 @@ const handler = async (m, {conn}) => {
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const str = `╭────[ *MichiBot* ]
 │
-│ *➤ ʜᴏʟᴀ ${taguser}*
+│ *┣・🧸 𝙃𝙔𝙋𝙀𝙍´𝙑 𝙏𝙀 𝙎𝘼𝙇𝙐𝘿𝘼 ${taguser}*
 │
-│ *=> 🤖 ᴛɪᴇᴍᴘᴏ ᴀᴄᴛɪᴠᴏ:* ${uptime}
-│ *=> ✅ ʙᴏᴛ ᴅᴇ ᴜsᴏ ᴘᴜʙʟɪᴄᴏ*
-│ *=> 👑 ᴄʀᴇᴀᴅᴏʀ: Richetti*
-│ *=> 🔗 ᴄᴜᴇɴᴛᴀs ᴏғᴄ:* https://www.instagram.com/ceogeripium.dzn
+│ *┣・🧸 𝙏𝙄𝙀𝙈𝙋𝙊 𝘼𝘾𝙏𝙄𝙑𝙊:* ${uptime}
+│ *┣・🧸 𝘽𝙊𝙏 𝘿𝙀 𝙐𝙎𝙊 𝙋𝘼𝙍𝘼 𝘾𝙊𝙈𝙋𝙍𝘼𝘿𝙊𝙍𝙀𝙎*
+│ *┣・🧸 𝘾𝙍𝙀𝘼𝘿𝙊𝙍: 𝘽𝙀𝙉𝙅𝘼𝙈𝙄𝙉*
+│ *┣・🧸 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝘿𝙀𝙇 𝘾𝙍𝙀𝘼𝘿𝙊𝙍:* https://www.instagram.com/boybenjx7
 ╰────────────────`.trim();
     if (m.isGroup) {
       conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://www.instagram.com/ceogeripium.dzn`, "sourceUrl": `https://www.instagram.com/ceogeripium.dzn`}}}, {quoted: m});
