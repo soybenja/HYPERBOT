38b5795758780fde2587310c1a187f8b152f02bf
𝙎𝙊𝙔 𝘽𝙀𝙉𝙅𝘼 <orteagalopezbenjamin@gmail.com>
2024-05-26 15:12:14 +1300

    Create vsteam495.js

---
+++ plugins/vsteam495.js
@@ -0,0 +1,28 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.vsteam495);
+};
+handler.command = /^(vsteam495|vsteam495)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.vsteam495 = ` 
+┏━━• *🇦🇱 Versus clk 🇦🇱* •━━┓
+┃🕚: 3:00 🇲🇽 4:00 🇨🇴
+┃
+┃🍭 *Jugadoras*
+┃🌷
+┃🌷
+┃🌷
+┃🌷
+┃🍭 *Suplentes*
+┃🌷
+┃🌷
+┃🪄 *Sala*
+┃🌷
+┃
+┃*Encargada:*
+┃
+┗━━━• 𝙃𝙔𝙋𝙀𝙍´𝙑 𝘽𝙊𝙏 •━━━┛
+`;
